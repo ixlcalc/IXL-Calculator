c45fa4d96e0a1c8bd88a38daf07035bfad6e4dad
ixlcalc <ixlcalc@protonmail.com>
2019-11-17 12:53:27 +0000

    Make functions collapsible

---
+++ util/collapse.js
@@ -0,0 +1,8 @@
+function collapse(element) {
+  var content = element.nextElementSibling;
+  if (content.style.display === "block") {
+    content.style.display = "none";
+  } else {
+    content.style.display = "block";
+  }
+}
